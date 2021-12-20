bbd805f994a1ed7cecae4161f066d7c0032747d6
taycrenshaw <95450001+taycrenshaw@users.noreply.github.com>
2021-12-19 23:46:40 -0600

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,39 @@
+#imported required modules
+import requests, json
+
+#API Key
+api_key = "d36d0dfdf07499efb48dbb4f1217d7bc"
+
+#base_url variable to store url
+base_url = "http://api.openweathermap.org/data/2.5/forecast?"
+
+#User inputs city name
+city_name = input("Please enter city name: ")
+
+#complete_url variable to store complete url address
+complete_url = base_url + "q=" + city_name + "&appid=" + api_key
+response = requests.get(complete_url)
+
+#checking the status code of the request
+if response["cod"] == 200:
+    #getting data in requests module
+    data = response.json()
+    #getting the main dict block
+    main = data['main']
+    #getting temperature
+    temperature = main['temp']
+    #getting the humidity
+    humidity = main['humidity']
+    #getting the pressure
+    pressure = main['pressure']
+    #weather report
+    report = data['weather']
+    print(f"{city_name: -^30}")
+    print(f"Temperature: {temperature}")
+    print(f"Humdity: {humidity}")
+    print(f"Pressure: {pressure}")
+    print(f"Weather Report: {report[0]['description']}")
+
+else:
+    #If there is an error message
+    print("Error in the HTTP request")
